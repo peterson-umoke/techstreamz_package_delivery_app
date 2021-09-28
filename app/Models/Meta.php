1686d5cfd1fdcf9aba0e25f2acd20b87b9ac263f
Peterson Umoke <umoke10@hotmail.com>
2021-09-28 23:39:38 +0100

    still setting things for meta

--- app/Models/Meta.php
+++ app/Models/Meta.php
@@ -2,8 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Meta extends Model
 {
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'group',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    public function metaable()
+    {
+        return $this->morphTo();
+    }
 }
